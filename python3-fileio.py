718f4177b4bd9e674a3f3bc6e9ee79783ea73913
YA <YA-androidapp@users.noreply.github.com>
2017-09-12 21:12:57 +0900

    add 2 sheets

---
+++ python3-fileio.py
@@ -0,0 +1,242 @@
+# ファイル操作
+
+# touch
+from pathlib import Path
+Path('test.txt').touch()
+
+# テキストファイル
+
+# 読込み
+file = open('test.txt', 'r')
+string = file.read()
+print(string)
+
+file = open('test.txt', 'r')
+string = file.readline()
+while string:
+    print(string)
+    string = file.readline()
+
+# 書込み
+file = open('test.txt', 'w')
+string = 'foobar hoge'
+file.write(string)
+
+file = open('test.txt', 'w')
+lst = ['foobar', 'hoge']
+file.writelines(lst)
+
+# 追記
+file = open('test.txt', 'a')
+string = 'foobar hoge'
+file.write(string)
+
+file = open('test.txt', 'a')
+lst = ['foobar', 'hoge']
+file.writelines(lst)
+
+# ファイルを閉じる
+file.close()
+
+# ファイルを閉じる(不要になったら自動的に解放)
+with open('test.txt', 'r') as file:  # with句
+    string = 'foobar hoge'
+    file.write(string)
+
+# CSV
+
+# 読込み
+import csv
+with open('test.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in csvreader:
+        print(', '.join(row))
+csvfile.close()
+
+# 書込み
+import csv
+with open('test.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['foo', 'bar', 'hoge'])
+    spamwriter.writerow(['foo', 'bar', 'hoge'])
+csvfile.close()
+csvfile.close()
+
+# 追記
+import csv
+with open('test.csv', 'a', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['foo', 'bar', 'hoge'])
+    spamwriter.writerow(['foo', 'bar', 'hoge'])
+csvfile.close()
+csvfile.close()
+
+# JSON
+
+# 読込み(JSONファイル)
+import json
+
+f = open('test.json', 'r')
+json_dict = json.load(f)
+print('json_dict:{}'.format(type(json_dict)))
+
+# 読込み(JSON文字列)
+import json
+
+json_str = '''
+{
+    "key1":"val1",
+    "key2":"val2"
+}
+'''
+
+json_dict = json.loads(json_str)
+print('json_dict:{}'.format(type(json_dict)))
+
+# 読込み(JSON文字列　順序を保つ)
+import collections
+import json
+
+json_str = '''
+{
+    "key1":"val1",
+    "key2":"val2"
+}
+'''
+
+decoder = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
+print(decoder.decode(json_str))
+
+# 要素の読込み
+import json
+
+f = open('test.json', 'r')
+json_dict = json.load(f)
+
+for x in json_dict:
+    num = json_dict[x]['num']
+
+    if(num >= 500):
+        print('{0}:{1}'.format(x, json_dict[x]))
+
+# 書込み
+import json
+
+# 書き出すオブジェクト
+data = {
+    'title': 'foobar',
+    'items': [
+        {
+            'title': '1',
+            'description': 'hoge'
+        },
+        {
+            'title': '2',
+            'description': 'hogehoge'
+        }
+    ]
+}
+
+savepath = 'test.json'
+with open(savepath, 'w') as outfile:
+    json.dump(data, outfile)
+
+# ディレクトリ操作
+
+# パスの生成
+import os.path
+path = os.path.join('C:\\', 'Python', 'scripts')
+
+# カレントディレクトリ
+import os
+os.getcwd()
+os.chdir(path)
+
+# 存在チェック
+import os
+path = 'C:\\'
+
+os.path.exists(path)  # ファイルとフォルダを区別しない
+
+os.path.isfile(path)  # ファイルの存在チェック
+
+os.path.isdir(path)  # フォルダの存在チェック
+
+# フォルダ作成
+import os
+os.mkdir(path)
+
+import os
+os.makedirs(os.path.join(path, 'sub'))
+
+# フォルダ名・ファイル名・拡張子の取得
+import os
+dirname = os.path.dirname(path)
+basename = os.path.basename(path)
+root, ext = os.path.splitext(path)
+
+# 絶対パスか検査・絶対パスを取得
+os.path.isabs('C:\\')
+os.path.abspath(".\\")
+
+# コピー
+path_old = ''
+path_new = ''
+
+import shutil
+shutil.copyfile(path_old, path_new)
+shutil.copytree(path_old, path_new)
+
+# リネーム・移動
+path_old = ''
+path_new = ''
+
+import os
+os.rename(path_old, path_new)
+
+shutil.move(path_old, path_new)
+
+# 削除
+import os
+os.remove(path)  # ファイルを削除
+
+import os
+os.rmdir(path)  # 空のディレクトリを削除
+
+import shutil
+shutil.rmtree(path)  # 再帰的に削除
+
+# ファイル走査
+
+# 直下のみ
+files = os.listdir(path)
+print(files)
+
+from glob import glob
+files = glob("*.txt")
+print(files)
+
+# 再帰
+import os
+
+
+def find_all_files(path):
+    for root, dirs, files in os.walk(path):
+        # yield root
+        for file in files:
+            if os.path.splitext(file)[1] == u'.dll':
+                yield os.path.join(root, file)
+
+
+for file in find_all_files(path):
+    print(file)
+
+# ファイルプロパティ
+os.path.getatime(path)  # 最終アクセス日時
+os.path.getmtime(path)  # 最終更新日時
+os.path.getsize(path)  # ファイルサイズ
+
+
+# Copyright (c) 2017 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
---
+++ python3-web.py
@@ -0,0 +1,322 @@
+# urllib
+
+# コンテンツを文字列として取得
+import urllib.request
+url = 'http://python.org/'
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+
+import urllib.request
+url = 'http://python.org/'
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+# コンテンツをテンポラリファイルとして取得
+import urllib.request
+url = 'http://python.org/'
+local_filename, headers = urllib.request.urlretrieve(url)
+html = open(local_filename)
+
+# バイナリファイルを保存
+import os
+import urllib.request
+url = 'http://python.org/'
+with urllib.request.urlopen(url) as response:
+    with open(os.path.basename(url), 'wb') as localfile:
+        localfile.write(response.read())
+
+# GET
+import urllib.parse
+import urllib.request
+
+url = 'http://python.org/'
+
+params = {}
+params['name'] = 'Sato'
+params['location'] = 'Tokyo'
+params['age'] = '30'
+query = urllib.parse.urlencode(params)
+url = url + '?' + query
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+
+# POST
+import urllib.parse
+import urllib.request
+
+url = 'http://python.org/'
+
+params = {'name': 'Sato', 'location': 'Tokyo',  'age': '30'}
+query = urllib.parse.urlencode(params)
+query = query.encode('ascii')
+
+req = urllib.request.Request(url, query)
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+# HTTPヘッダ(headers引数)
+import urllib.parse
+import urllib.request
+
+url = 'http://python.org/'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/xxx.xx (KHTML, like Gecko) Chrome/xx.x.xxxx.xx Safari/xxx.xx'
+headers = {'User-Agent': user_agent}
+
+params = {'name': 'Sato', 'location': 'Tokyo',  'age': '30'}
+query = urllib.parse.urlencode(params)
+query = query.encode('ascii')
+
+req = urllib.request.Request(url, query, headers)
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+# HTTPヘッダ(add_header)
+import urllib.parse
+import urllib.request
+
+url = 'http://python.org/'
+
+params = {'name': 'Sato', 'location': 'Tokyo',  'age': '30'}
+query = urllib.parse.urlencode(params)
+query = query.encode('ascii')
+
+req = urllib.request.Request(url, query)
+req.add_header('Referer', 'http://www.python.org/')
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+# BASIC認証
+import urllib.request
+import urllib.request
+import getpass
+
+url = 'http://python.org/'
+auth_user = 'Username'
+auth_passwd = 'Password'
+
+passman = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+# If we knew the realm, we could use it instead of None.
+passman.add_password(None, url, auth_user, auth_passwd)
+# HTTPBasicAuthHandler or HTTPDigestAuthHandler
+authhandler = urllib.request.HTTPBasicAuthHandler(passman)
+opener = urllib.request.build_opener(authhandler)
+urllib.request.install_opener(opener)
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+
+# 応答ヘッダ・リダイレクト先URL
+import urllib.parse
+import urllib.request
+
+url = 'http://python.org/'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/xxx.xx (KHTML, like Gecko) Chrome/xx.x.xxxx.xx Safari/xxx.xx'
+headers = {'User-Agent': user_agent}
+
+params = {'name': 'Sato', 'location': 'Tokyo',  'age': '30'}
+query = urllib.parse.urlencode(params)
+query = query.encode('ascii')
+
+req = urllib.request.Request(url, query, headers)
+with urllib.request.urlopen(req) as response:
+    url = response.geturl()
+    headers = response.info()
+    print(headers)
+    # charset=req.info().get_content_charset() # 応答ヘッダから文字コードを取得してデコードする例
+    # content=req.read().decode(charset)
+
+# 例外処理とレスポンスコード
+import urllib.parse
+import urllib.request
+from urllib.error import URLError
+
+url = 'http://python.org/'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/xxx.xx (KHTML, like Gecko) Chrome/xx.x.xxxx.xx Safari/xxx.xx'
+headers = {'User-Agent': user_agent}
+
+params = {'name': 'Sato', 'location': 'Tokyo',  'age': '30'}
+query = urllib.parse.urlencode(params)
+query = query.encode('ascii')
+
+req = urllib.request.Request(url, query, headers)
+try:
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+except URLError as e:  # URLErrorはHTTPErrorも拾う
+    print(e.code)
+    print(e.read())
+
+    if hasattr(e, 'reason'):
+        print('Reason: ', e.reason)
+    elif hasattr(e, 'code'):
+        print('Error code: ', e.code)
+else:
+    pass  # リクエストに成功
+
+
+# Requests
+
+# pip install requests
+
+# GET
+import requests
+url = 'http://python.org/'
+requests.get(url)
+
+# POST
+import requests
+url = 'http://python.org/'
+requests.post(url)
+
+# PUT
+import requests
+url = 'http://python.org/'
+requests.put(url)
+
+# DELETE
+import requests
+url = 'http://python.org/'
+requests.delete(url)
+
+# header の取得
+import requests
+url = 'http://python.org/'
+requests.head(url)
+
+# クエリ
+
+# GET
+import requests
+url = 'http://python.org/'
+payload = {'key1': 'val1', 'key2': 'val2'}
+r = requests.get(url, params=payload)
+print(r.url)  # 生成されたURL
+
+# POST
+import requests
+url = 'http://python.org/'
+payload = {'key1': 'val1', 'key2': 'val2'}
+r = requests.post(url, data=payload)
+print(r.url)  # 生成されたURL(POSTなのでクエリ文字列がないことを確認)
+
+# ヘッダの追加
+import requests
+url = 'http://python.org/'
+payload = {'key1': 'val1', 'key2': 'val2'}
+headers = {'Referer', 'http://www.python.org/'}
+r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+# フォーム送信(Multipartエンコード)
+import requests
+url = 'http://python.org/'
+files = {'file': open('test.png', 'rb')}
+r = requests.post(url, files=files)
+
+import requests
+url = 'http://python.org/'
+files = {'file': ('test.png', open('test.png', 'rb'))}
+r = requests.post(url, files=files)
+
+import requests
+url = 'http://python.org/'
+files = {'file': ('test.txt', 'foobar')}
+r = requests.post(url, files=files)
+
+# 応答
+import requests
+url = 'http://python.org/'
+r = requests.get(url)
+r.headers
+# r.headers['status']
+# r.headers.get('status')
+r.text
+
+import requests
+url = 'http://python.org/'
+r = requests.get(url)
+r.status_code  # レスポンスコード
+r.status_code == requests.codes.ok  # 200か判定
+
+import requests
+url = 'http://python.org/'
+r = requests.get(url)
+r.encoding  # 文字エンコードの確認
+r.encoding = 'Shift-JIS'  # 文字コードの設定(変更)
+r.text  # 変更後のエンコーディングが使用される
+
+# Cookie
+
+# 取得
+import requests
+url = 'http://python.org/'
+r = requests.get(url)
+r.cookies['key1']  # Cookieが存在する場合は非None
+
+# 設定
+import requests
+url = 'http://python.org/'
+cookies = dict(key1='val1')
+r = requests.get(url, cookies=cookies)
+
+# リダイレクト禁止
+import requests
+url = 'http://python.org/'
+r = requests.get(url, allow_redirects=True)
+
+# タイムアウト
+import requests
+url = 'http://python.org/'
+r = requests.get(url, timeout=1)
+
+
+# 画像ファイルの保存
+
+# pip install Image
+# pip install requests
+# pip install StringIO
+
+import os
+import requests
+from PIL import Image
+from io import BytesIO
+url = 'https://www.python.org/static/img/python-logo.png'
+r = requests.get(url)
+i = Image.open(BytesIO(r.content))
+i.save(os.path.basename(url))
+
+# 大容量ファイルの保存
+import os
+import requests
+url = 'https://www.python.org/static/img/python-logo.png'
+res = requests.get(url, stream=True)
+if res.status_code == 200:
+    with open(os.path.basename(url), 'wb') as file:
+        for chunk in res.iter_content(chunk_size=1024):
+            file.write(chunk)
+
+# JSON
+import requests
+url = 'http://python.org/path/to/json/'
+r = requests.get()
+r.json()
+
+# セッション
+import requests
+url = 'http://python.org/path/to/json/'
+session = requests.session()
+auth_data = {'username': 'foo', 'password': 'bar'}
+res = session.post(url, data=auth_data)
+res = session.post(url, data={'key1': 'val1'})
+
+# 例外処理とレスポンスコード
+import requests
+url = 'http://python.org/'
+try:
+    r = requests.get(url)
+except requests.exceptions.RequestException as e:
+    print("Error: {}".format(e))
+
+
+# Copyright (c) 2017 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
