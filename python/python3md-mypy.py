46229a18c7266ab0c7af4aa0bba4dcc4d8d41689
YA <YA-androidapp@users.noreply.github.com>
2020-10-31 09:29:43 +0900

    mypy

--- python/python3md-mypy.py
+++ python/python3md-mypy.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from typing import Optional
+from typing import Union
+from typing import List, Dict
+
+###
+
 boolvar: bool = True
 intvar: int = 123
 floatvar: float = 1.23
@@ -8,3 +14,20 @@ strvar: str = 'foo'
 
 strvar = 'bar'
 boolvar = 'hoge'
+
+###
+
+# # listvar: list = [1, 2, 3]
+# # dictvar: dict = {1: 'foo', 2: 'bar'}
+# listvar: List[int] = [1, 2, 3]
+# dictvar: Dict[int, str] = {1: 'foo', 2: 'bar'}
+
+# intstrvar: Union[int, str] = 123
+# intstrvar = 'foo'
+# intstrvar = 1.23
+
+###
+
+dictvar: Dict[int, str] = {1: 'foo'}
+strnonevar: Optional[str] = dictvar.get(2)
+print(strnonevar)
