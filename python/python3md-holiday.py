79825ca289d40c400551f0f80b01cb3dd900a45a
YA <YA-androidapp@users.noreply.github.com>
2022-02-24 21:59:49 +0900

    gen

---
+++ python/python3md-holiday.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+# Required:
+#   pip install requests
+
+
+import requests
+import sys
+
+
+url = 'https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv'
+
+
+def get_holidays(date_label):
+    response = requests.get(url)
+    print('response.status_code', response.status_code)
+    if response.status_code == 200:
+        response.encoding = 'Shift_JIS'
+        holidays = response.text.splitlines()
+
+        # ヘッダ行削除
+        holidays.pop(0)
+
+        # 前方一致でフィルタリング
+        if date_label != '':
+            holidays = list(filter(lambda x: x.startswith(date_label), holidays))
+
+        return [item.split(',') for item in holidays]
+
+
+if __name__ == "__main__":
+    date_label = ''
+    if len(sys.argv) == 2:
+        date_label = sys.argv[1]
+
+    result = get_holidays(date_label)
+    print(result)
