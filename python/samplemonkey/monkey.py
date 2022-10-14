8eaf960091e40b37d3f8e2e9b01ab72ac7728926
YA <YA-androidapp@users.noreply.github.com>
2022-10-15 08:55:39 +0900

    updates

--- python/samplemonkey/monkey.py
+++ python/samplemonkey/monkey.py
@@ -1,5 +1,11 @@
-def echo(message: str):
-    return 'echo: ' + message
+# def echo(message: str):
+#     return 'echo: ' + message
+#
+# def main():
+#     return echo('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
 
-def main():
-    return echo('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+
+# --------------------
+
+def __private_function():
+    return '__private_function()'
--- python/samplemonkey/test_monkey.py
+++ python/samplemonkey/test_monkey.py
@@ -1,21 +1,58 @@
-import pytest
+# import pytest
+#
+#
+# import sys
+# current_module = sys.modules[__name__]
+#
+#
+# def mock_echo(message: str):
+#     return 'mock_echo'
+#
+# def main():
+#     return echo('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+#
+# def test_main(monkeypatch: pytest.MonkeyPatch):
+#     monkeypatch.setattr(current_module, 'main', mock_echo)
 
 
-import monkey
+# --------------------
 
 
-def mock_echo(message: str):
-    return 'mock_echo'
+# import pytest
+#
+#
+# import monkey
+#
+#
+# def mock_echo(message: str):
+#     return 'mock_echo'
+#
+#
+# def mock_main():
+#     return 'mock_main'
+#
+#
+# def test_main(monkeypatch: pytest.MonkeyPatch):
+#     # monkeypatch.setattr(monkey, 'echo', lambda *_: 'mock_echo')
+#     monkeypatch.setattr(monkey, 'echo', mock_echo)
+#     monkeypatch.setattr(monkey, 'main', mock_main)
+#
+#     assert monkey.echo('') == 'mock_echo'
+#     assert monkey.main() == 'mock_main'
 
 
-def mock_main():
-    return 'mock_main'
+# --------------------
 
 
-def test_main(monkeypatch: pytest.MonkeyPatch):
-    # monkeypatch.setattr(monkey, 'echo', lambda *_: 'mock_echo')
-    monkeypatch.setattr(monkey, 'echo', mock_echo)
-    monkeypatch.setattr(monkey, 'main', mock_main)
+# プライベート関数の場合は import <module> ではなく `as <alias>` でエイリアスを付ける必要がある
+# from monkey import __private_function as pf
+import monkey as pf
 
-    assert monkey.echo('') == 'mock_echo'
-    assert monkey.main() == 'mock_main'
+
+import sys
+current_module = sys.modules[__name__]
+
+
+def test_private_function(monkeypatch):
+    monkeypatch.setattr(current_module, 'pf', lambda: 'MOCKED')
+    assert pf() == 'MOCKED'
