70ae60b0d79097a77f281ed21c47d0ff5bbd237e
YA <YA-androidapp@users.noreply.github.com>
2023-06-21 19:41:18 +0900

    ttc2ttf

---
+++ python/python3md-font.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+# Required:
+#   pip install fonttools
+
+
+from fontTools.ttLib import TTCollection
+import os
+
+
+ttc_path = './YuGothM.ttc'
+output_dir = './ttf/'
+os.makedirs(output_dir, exist_ok=True)
+
+
+ttc = TTCollection(ttc_path)
+for i, ttf in enumerate(ttc):
+    output_path = output_dir + f'meiryo_{i}.ttf'
+    ttf.save(output_path)
